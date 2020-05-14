15f2294262c6e0c166e3195b5250d8f86d2069f6
Ramon Geronimo <rgero215@gmail.com>
2020-05-14 12:00:26 -0700

    debug true

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -5,6 +5,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEBUG = True
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
